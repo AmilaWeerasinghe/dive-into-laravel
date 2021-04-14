e2c99412c8f1f71b48fd3a6af2008e53a40cd7a2
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-04-14 07:43:47 +0530

    new controller

---
+++ app/Http/Controllers/newTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class newTest extends Controller
+{
+    //
+    function index()
+    {
+        echo "new controller";
+    }
+}
